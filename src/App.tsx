27b8e08e131b16f0df03cca08487174d8a4da3a2
Capsaicin1 <capsaicin1@outlook.com>
2024-06-06 10:33:16 +1200

    Added Data Source
        -Added data source and checked if source is properly loaded,
        -Updated css

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import React, { useState, useRef, useEffect } from "react";
 import "./App.css";
-
 import mapboxgl from "mapbox-gl";
-
 mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_API;
 
+// Main functional component that renders the map
 function App() {
   const mapContainer = useRef<any>(null);
   const map = useRef<mapboxgl.Map | null>(null);
@@ -13,19 +12,38 @@ function App() {
   const [zoom, setZoom] = useState(4);
   const [pitch, setPitch] = useState(52);
   const [bearing, setBearing] = useState(-9);
-  //const style = "mapbox://styles/julesishomie/clwycze8h019801pp1qto1bwq";
+  const style = "mapbox://styles/julesishomie/clwycze8h019801pp1qto1bwq";
 
   useEffect(() => {
-    if (map.current) return; //initialize map only once
+    //initialize map only once
+    if (map.current) return;
+    // Creates the map and customizes basic properties
     map.current = new mapboxgl.Map({
       container: mapContainer.current,
-      style: "mapbox://styles/julesishomie/clwycze8h019801pp1qto1bwq",
+      style,
       center: [lng, lat],
       zoom,
       pitch,
       bearing,
+      maxZoom: 9,
+      minZoom: 0,
     });
 
+    map.current.on("load", () => {
+      if (map.current !== null) {
+        map.current.addSource("NTLChangeAbs", {
+          type: "raster",
+          url: "mapbox://julesishomie.2wpkql9d",
+          tileSize: 256,
+        });
+
+        map.current.on("data", (e) => {
+          if (e.sourceId === "NTLChangeAbs" && e.isSourceLoaded)
+            console.log("Raster source is loaded 🚀");
+        });
+      } else console.log("map.current == null");
+    });
+    // Gets and displays the following values as the user moves the map by updating the state of each variable
     map.current.on("move", () => {
       if (map.current !== null) {
         setLng(parseInt(map.current.getCenter().lng.toFixed(4)));
