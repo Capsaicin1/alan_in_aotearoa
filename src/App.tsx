a89163d27077da66fd5df5fd782883fe6ff47a4f
Capsaicin1 <capsaicin1@outlook.com>
2024-06-12 21:40:10 +1200

    Data Visualization Started & Minor Adjustments
    - Minor adjustments to state var names.
    - First attempt to visualize the raster data.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,61 @@
-import React, { useState, useRef, useEffect } from "react";
+import { useState, useRef, useEffect } from "react";
 import "./App.css";
 import mapboxgl from "mapbox-gl";
 mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_API;
 
-// Main functional component that renders the map
+/**
+ * Main component that renders all of the content on the page.
+ * Custom components are made in a separate file and imported.
+ */
 function App() {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const mapContainer = useRef<any>(null);
   const map = useRef<mapboxgl.Map | null>(null);
-  const [lng, setLng] = useState(172);
-  const [lat, setLat] = useState(-41);
-  const [zoom, setZoom] = useState(4);
-  const [pitch, setPitch] = useState(52);
-  const [bearing, setBearing] = useState(-9);
+  const [defaultLng, setDefaultLng] = useState(172);
+  const [defaultLat, setDefaultLat] = useState(-41);
+  const [defaultZoom, setDefaultZoom] = useState(4);
+  const [defaultPitch, setDefaultPitch] = useState(52);
+  const [defaultBearing, setDefaultBearing] = useState(-9);
   const style = "mapbox://styles/julesishomie/clwycze8h019801pp1qto1bwq";
+  const colors = {
+    gradient: [
+      "#0000FF",
+      "#00FF00",
+      "#7FFF7F",
+      "#FFFFFF",
+      "#FF7F7F",
+      "#FF0000",
+      "#FF00FF",
+    ],
+  };
 
+  /**
+   * Resets the map view to the specified parameters or default values.
+   *
+   * @param lng - Longitudinal pos on map.
+   * @param lat - Latitudinal pos on map.
+   * @param pitch - Camera's pitch.
+   * @param zoom - Map zoom level.
+   * @param bearing - Map bearing.
+   *
+   * '?' Allows param to be optional.
+   * This function is kinda broken!!!!
+   */
+  const resetMapView = (
+    lng?: number,
+    lat?: number,
+    pitch?: number,
+    zoom?: number,
+    bearing?: number
+  ): void => {
+    if (map.current) {
+      map.current
+        .setCenter([lng ?? defaultLng, lat ?? defaultLat])
+        .setPitch(pitch ?? defaultPitch)
+        .setZoom(zoom ?? defaultZoom)
+        .setBearing(bearing ?? defaultBearing);
+    }
+  };
   useEffect(() => {
     //initialize map only once
     if (map.current) return;
@@ -21,49 +63,91 @@ function App() {
     map.current = new mapboxgl.Map({
       container: mapContainer.current,
       style,
-      center: [lng, lat],
-      zoom,
-      pitch,
-      bearing,
+      center: [defaultLng, defaultLat],
+      zoom: defaultZoom,
+      pitch: defaultPitch,
+      bearing: defaultBearing,
       maxZoom: 9,
       minZoom: 0,
     });
 
     map.current.on("load", () => {
-      if (map.current !== null) {
-        map.current.addSource("NTLChangeAbs", {
-          type: "raster",
-          url: "mapbox://julesishomie.2wpkql9d",
-          tileSize: 256,
-        });
+      if (map.current === null) return;
+      map.current.addSource("NTLChangeAbs_tileset", {
+        type: "raster",
+        url: "mapbox://julesishomie.2wpkql9d",
+        tileSize: 256,
+      });
 
-        map.current.on("data", (e) => {
-          if (e.sourceId === "NTLChangeAbs" && e.isSourceLoaded)
-            console.log("Raster source is loaded 🚀");
-        });
-      } else console.log("map.current == null");
+      //Checks that the raster is loaded before adding it to the map.
+      map.current.on("data", (e) => {
+        if (e.sourceId === "NTLChangeAbs_tileset" && e.isSourceLoaded) {
+          map.current?.addLayer({
+            id: "NTLChangeAbs",
+            source: "NTLChangeAbs_tileset",
+            type: "raster",
+            paint: {
+              "raster-color": [
+                "interpolate",
+                ["linear"],
+                ["raster-value"],
+                0,
+                colors.gradient[0],
+                23,
+                colors.gradient[1],
+                46,
+                colors.gradient[2],
+                69,
+                colors.gradient[3],
+                92,
+                colors.gradient[4],
+                115,
+                colors.gradient[5],
+                138,
+                colors.gradient[6],
+              ],
+              "raster-color-mix": [255, 0, 0, 0],
+              "raster-color-range": [0, 138],
+            },
+          });
+
+          console.log("Raster source is loaded 🚀" + e.isSourceLoaded);
+        } else console.log("Raster could not be loaded 🤷");
+      });
     });
+
     // Gets and displays the following values as the user moves the map by updating the state of each variable
     map.current.on("move", () => {
       if (map.current !== null) {
-        setLng(parseInt(map.current.getCenter().lng.toFixed(4)));
-        setLat(parseInt(map.current.getCenter().lat.toFixed(4)));
-        setZoom(parseInt(map.current.getZoom().toFixed(2)));
-        setPitch(parseInt(map.current.getPitch().toFixed(2)));
-        setBearing(parseInt(map.current.getBearing().toFixed(3)));
+        setDefaultLng(parseInt(map.current.getCenter().lng.toFixed(4)));
+        setDefaultLat(parseInt(map.current.getCenter().lat.toFixed(4)));
+        setDefaultZoom(parseInt(map.current.getZoom().toFixed(2)));
+        setDefaultPitch(parseInt(map.current.getPitch().toFixed(2)));
+        setDefaultBearing(parseInt(map.current.getBearing().toFixed(3)));
       }
     });
 
+    /**
+     * Disables zoom with mouse/track pad and adds zoom buttons.
+     * If the map takes up the whole page, you wont be able to scroll down without doing this.
+     */
     map.current.scrollZoom.disable();
     map.current.addControl(new mapboxgl.NavigationControl(), "top-right");
+
+    // return () => {
+    //   map.current = null;
+    // };
   });
 
   return (
     <>
       <div className="sidebar">
-        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom} | Pitch: {pitch} |
-        Bearing: {bearing}
+        Longitude: {defaultLng} | Latitude: {defaultLat} | Zoom: {defaultZoom} |
+        Pitch:{defaultPitch} | Bearing: {defaultBearing}
       </div>
+      <button onClick={() => resetMapView(defaultLat, defaultLng)}>
+        Reset Map View
+      </button>
       <div ref={mapContainer} className="map-container" />
     </>
   );
